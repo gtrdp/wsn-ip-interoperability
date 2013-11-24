944e8d6283f616831e3e3fce9a093f41d58e0f56
Guntur Dharma Putra <guntur.dharma@gmail.com>
2013-11-25 06:07:06 +0700

    Add mode.php

---
+++ web/mode.php
@@ -0,0 +1,35 @@
+<?php include('pages/header.php'); ?>
+
+<div class="container-fluid">
+    <div class="row-fluid">
+        <?php include('pages/sidebar.php'); ?>
+		
+		<div class="span9" id="content">
+			<div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+                            <div class="navbar navbar-inner block-header">
+                                <div class="muted pull-left">jQuery Knob</div>
+                                <div class="pull-right"><span class="badge badge-warning">View More</span>
+
+                                </div>
+                            </div>
+                            <div class="block-content collapse in">
+                                
+                            </div>
+                        </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+        </div>
+
+
+    </div>
+</div>
+</div>
+<hr>
+
+
+<?php include('pages/footer.php'); ?>
